43b547adf0adbdfb65d11ddb6da37ed0a568cbc5
black <black@example.com>
2022-11-06 21:38:32 +0500

    Add #BLM

--- hello.py
+++ hello.py
@@ -2,6 +2,10 @@ def greetWhite():
     print("hello white")
 
 def greetBlack():
-    print("hello black")
+    print("#BlackLivesMatter")
+    print("I swear I will never use the n-word.")
+    print("All races are equal.")
+    print("The right of citizens of the United States to vote shall not be denied or abridged by the United States or by any State on account of race, color, or previous condition of servitude.")
+    print("hello black guy:)")
 
 print("hello everyone")
